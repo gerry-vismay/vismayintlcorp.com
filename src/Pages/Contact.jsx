bf3d76d7c4dea8fbf594b58f28a415ff65c91d21
gerrysantos <gerrysantos0226@gmail.com>
2025-04-30 14:05:28 +0800

    update contact to mobile view

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -52,14 +52,14 @@ const Contact = () => {
     }, 10000); // 10 seconds
   };
   return (
-    <section className="max-w-screen-2xl mx-auto py-24">
-      <h1 className="text-center font-bold text-5xl mb-10">
+    <section className="max-w-screen-2xl mx-auto py-12 lg:py-24">
+      <h1 className="lg:block hidden lg:text-center lg:font-bold lg:text-5xl lg:mb-10">
         How would you like to contact us?
       </h1>
-      <div className="flex justify-between gap-16 ">
+      <div className="flex flex-col lg:flex-row lg:justify-between gap-16 mx-12">
         <div className="bg-gray-200 px-8 py-4 rounded-lg">
-          <h2 className="text-3xl font-semibold">Request a call</h2>
-          <p className="mb-10 text-xl">
+          <h2 className="text-lg md:text-3xl font-semibold">Request a call</h2>
+          <p className="mb-10 lg:text-xs md:text-xl">
             Give us some info so the right person can get back to you
           </p>
 
@@ -71,15 +71,15 @@ const Contact = () => {
           ) : (
             <form
               onSubmit={handleSubmit}
-              className="space-y-4 flex flex-col gap-y-10 text-xl"
+              className=" flex flex-col gap-y-10 text-md lg:text-xl"
             >
-              <div className="flex gap-5 ">
+              <div className="flex flex-col lg:flex-row lg:gap-5 ">
                 <input
                   type="text"
                   name="firstname"
                   id="first-name"
                   placeholder="First Name"
-                  className="border border-black px-4 py-2 grow"
+                  className="border border-black px-4 py-2 grow mb-10 lg:mb-0"
                   value={formData.firstname}
                   onChange={handleChange}
                   required
@@ -147,12 +147,12 @@ const Contact = () => {
         </div>
         <div className="flex flex-col gap-14">
           <div className="bg-gray-200 px-8 py-10 rounded-lg">
-            <h2 className="text-3xl">Chat With Us</h2>
-            <p className="text-xl mb-14 mt-4">
+            <h2 className="text-lg md:text-3xl font-bold">Chat With Us</h2>
+            <p className="lg:text-xs md:text-xl mb-14 mt-4">
               Get product info, login help, and live chat with agent.
             </p>
             <button
-              className="bg-[#07549C] text-white text-2xl py-2 px-16"
+              className="bg-[#07549C] text-white text-lg lg:text-2xl py-2 px-16"
               type="submit"
             >
               Let's Chat
@@ -160,12 +160,14 @@ const Contact = () => {
           </div>
 
           <div className="bg-gray-200 px-8 py-10 rounded-lg">
-            <h2 className="text-3xl">Leave us some feedback</h2>
-            <p className="text-xl mb-14 mt-4">
+            <h2 className="text-lg md:text-3xl font-bold">
+              Leave us some feedback
+            </h2>
+            <p className="lg:text-xs md:text-xl text-xl mb-14 mt-4">
               Good or bad, we love to hear it all.
             </p>
             <button
-              className="bg-[#07549C] text-white text-2xl py-2 px-16"
+              className="bg-[#07549C] text-white text-lg lg:text-2xl py-2 px-16"
               type="submit"
             >
               Send Feedback
