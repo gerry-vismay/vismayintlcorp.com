540f50fd786c8e04a4fb4df0ae9b60f310b1246a
gerrysantos <gerrysantos0226@gmail.com>
2025-04-30 13:09:45 +0800

    update join our growing team font

--- src/Pages/Career.jsx
+++ src/Pages/Career.jsx
@@ -14,7 +14,9 @@ const Career = () => {
       <Banner desktopImage={careerImg} mobileImage={careerMobile} />
 
       <div className="text-center py-16">
-        <p className="uppercase text-5xl font-bold">join our growing team</p>
+        <p className="uppercase text-3xl lg:text-5xl font-bold">
+          join our growing team
+        </p>
       </div>
       <div className="max-w-screen-xl mx-auto mb-20 ">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-16">
