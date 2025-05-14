069564e1a45c13a0c57845360251f0bd4e28975e
gerrysantos <gerrysantos0226@gmail.com>
2025-05-14 16:39:45 +0800

    replace jobopen to data/

--- server/models/itemModel.js
+++ server/models/itemModel.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 const path = require("path");
 
-const DATA_FILE = path.join(__dirname, "../jobOpen.json");
+const JOB_FILE = path.join(__dirname, "./../data/jobOpen.json");
 
 function readData() {
-  return JSON.parse(fs.readFileSync(DATA_FILE, "utf-8"));
+  return JSON.parse(fs.readFileSync(JOB_FILE, "utf-8"));
 }
 
 function writeData(data) {
-  fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
+  fs.writeFileSync(JOB_FILE, JSON.stringify(data, null, 2));
 }
 
 module.exports = {
