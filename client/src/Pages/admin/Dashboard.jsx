0554be8a5b6d3213958ca65d5fd066d6a5e042fe
gerrysantos <gerrysantos0226@gmail.com>
2025-05-14 16:33:43 +0800

    update table from dashboard

--- client/src/Pages/admin/Dashboard.jsx
+++ client/src/Pages/admin/Dashboard.jsx
@@ -35,48 +35,52 @@ export default function Dashboard() {
     <div className="px-4 ">
       <h2 className="text-2xl font-bold mb-4">DASHBOARD</h2>
       <table className="w-full border border-collapse border-solid border-blue-gray-100 text-xs">
-        <tr className="border border-solid border-blue-gray-100">
-          <th>ID</th>
-          <th>Position</th>
-          <th>Department</th>
-          <th>Qualifications</th>
-          <th>Location</th>
-          <th>Status</th>
-        </tr>
-        {items
-          .filter((item) => {
-            return item.status === "Active";
-          })
-          .map((item) => (
-            <tr>
-              <td className="text-center w-[3%] border border-solid border-blue-gray-100">
-                {item.id}
-              </td>
-              <td className="text-center border border-solid border-blue-gray-100 w-[9%]">
-                {item.position}
-              </td>
-              <td className="text-center border border-solid border-blue-gray-100 w-[9%]">
-                {item.department}
-              </td>
-              <td className="px-5 border border-solid border-blue-gray-100 w-[35%] ">
-                <ul className="list-disc">
-                  {item.qualifications.map((requirement) => (
-                    <li>{requirement}</li>
-                  ))}
-                </ul>
-              </td>
-              <td className="px-5 border border-solid border-blue-gray-100 w-[32%] ">
-                <ul className="list-disc">
-                  {item.locations.map((loc) => {
-                    return <li>{loc}</li>;
-                  })}
-                </ul>
-              </td>
-              <td className="text-center border border-solid border-blue-gray-100 w-[7%]">
-                {item.status}
-              </td>
-            </tr>
-          ))}
+        <thead className="border border-solid border-blue-gray-100">
+          <tr>
+            <th>ID</th>
+            <th>Position</th>
+            <th>Department</th>
+            <th>Qualifications</th>
+            <th>Location</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items
+            .filter((item) => {
+              return item.status === "Active";
+            })
+            .map((item) => (
+              <tr key={item.id}>
+                <td className="text-center w-[3%] border border-solid border-blue-gray-100">
+                  {item.id}
+                </td>
+                <td className="text-center border border-solid border-blue-gray-100 w-[9%]">
+                  {item.position}
+                </td>
+                <td className="text-center border border-solid border-blue-gray-100 w-[9%]">
+                  {item.department}
+                </td>
+                <td className="px-5 border border-solid border-blue-gray-100 w-[35%] ">
+                  <ul className="list-disc">
+                    {item.qualifications.map((requirement, i) => (
+                      <li key={i}>{requirement}</li>
+                    ))}
+                  </ul>
+                </td>
+                <td className="px-5 border border-solid border-blue-gray-100 w-[32%] ">
+                  <ul className="list-disc">
+                    {item.locations.map((loc, i) => {
+                      return <li key={i}>{loc}</li>;
+                    })}
+                  </ul>
+                </td>
+                <td className="text-center border border-solid border-blue-gray-100 w-[7%]">
+                  {item.status}
+                </td>
+              </tr>
+            ))}
+        </tbody>
       </table>
     </div>
   );
