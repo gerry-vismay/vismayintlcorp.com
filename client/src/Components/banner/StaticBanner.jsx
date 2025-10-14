389f334cc4297ad229abc9935b9e1e7ef6eb58f4
gerrysantos <gerrysantos0226@gmail.com>
2025-10-14 13:02:12 +0800

    update our brands responsive

--- client/src/Components/banner/StaticBanner.jsx
+++ client/src/Components/banner/StaticBanner.jsx
@@ -1,23 +1,39 @@
 import React from "react";
 
-const Banner = ({ link, label, desktopImage, mobileImg, className = "" }) => {
+const Banner = ({ link, alt, img, mobile, className = "" }) => {
   return (
-    <div>
+    // <div>
+    //   <a href={link}>
+    //     <div className="hidden lg:block">
+    //       <img
+    //         src={desktopImage}
+    //         alt={label}
+    //         className={`h-full w-full object-cover relative py-7 ${className}`}
+    //       />
+    //     </div>
+    //     <div className="lg:hidden block">
+    //       <img
+    //         src={mobileImg}
+    //         alt={label}
+    //         className={`h-full w-full object-cover relative py-7 ${className}`}
+    //       />
+    //     </div>
+    //   </a>
+    // </div>
+
+    <div className={`${className}`}>
       <a href={link}>
-        <div className="hidden lg:block">
+        <picture className=" ">
+          <source media="(min-width: 768px)" srcset={mobile}></source>
+          <img src={img} alt={alt} className={`h-full w-full object-cover `} />
+        </picture>
+        {/* <div className="lg:hidden block ">
           <img
-            src={desktopImage}
-            alt={label}
-            className={`h-full w-full object-cover relative py-7 ${className}`}
+            src={mobileImage}
+            alt=""
+            className={`h-full w-full object-cover `}
           />
-        </div>
-        <div className="lg:hidden block">
-          <img
-            src={mobileImg}
-            alt={label}
-            className={`h-full w-full object-cover relative py-7 ${className}`}
-          />
-        </div>
+        </div> */}
       </a>
     </div>
   );
--- client/src/Pages/OurBrand.jsx
+++ client/src/Pages/OurBrand.jsx
@@ -3,54 +3,35 @@ import pk from "../assets/images/brand/pk.webp";
 import lf from "../assets/images/brand/laifen.webp";
 
 import Banner from "../components/banner/Banner";
+import SectionBanner from "../components/banner/Banner";
 
 const OurBrand = () => {
   return (
     <div className="flex flex-col items-center min-h-screen bg-gray-50 pb-10">
       {/* Photo Holder */}
-      <Banner img={BRAND} />
+      <Banner img={BRAND} alt="Our Brand Section" />
 
       {/* Main Content */}
-      <div className="max-w-[1588px] w-full mx-auto mt-20 !mb-10">
-        {/* Top Section */}
-        <div className="!mt-15">
-          <h2 className="mb-10 mt-6 font-inter font-bold text-[#06529B] text-left w-[1062px] h-[77px] text-[36px] leading-[1] ">
+      <div className="max-w-[1440px] w-full mx-auto mt-20 mb-10 px-4">
+        {/* Platinum Karaoke Section */}
+        <div className="mt-15 ">
+          <h2 className="mb-10 mt-6 font-bold text-[#06529B] text-lg md:text-4xl leading-[1] ">
             The Platinum
           </h2>
 
           <div className="w-full flex justify-center mb-12">
-            <img
-              src={pk}
-              alt="The Platinum"
-              className="w-full max-w-[1588px] h-auto object-cover"
-            />
+            <SectionBanner img={pk} alt="Platinum Karaoke section Banner" />
           </div>
         </div>
 
-        {/* Bottom Section */}
-        <div className="!mt-15">
-          <h2
-            className="
-                mb-10
-                mt-6
-                font-inter font-bold
-                text-[#06529B]
-                text-left
-                w-[1062px]
-                h-[77px]
-                text-[36px]
-                leading-[1]
-                "
-          >
+        {/* Laifen Section */}
+        <div className="mt-15">
+          <h2 className="mb-10 mt-6 font-bold text-[#06529B] text-lg md:text-4xl leading-[1]">
             Laifen
           </h2>
 
           <div className="w-full flex justify-center">
-            <img
-              src={lf}
-              alt="Laifen"
-              className="w-full max-w-[1588px] h-auto object-cover"
-            />
+            <SectionBanner img={lf} alt="Laifen section Banner" />
           </div>
         </div>
       </div>
